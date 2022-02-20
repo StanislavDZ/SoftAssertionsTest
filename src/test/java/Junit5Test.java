dd42de5f87feb0490d4831aadebbce3efec6aef2
Kashtos90 <29061990a@gmail.com>
2022-02-20 13:43:12 +0700

    added config

---
+++ src/test/java/Junit5Test.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Junit5Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void shouldFindJunit5Code(){
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+
+    }
+}
+
